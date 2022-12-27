1b289bc4cb50649b4707324aa90f2d8f0b4e64e7
Adrian Rafly <adrianrafly.ar@gmail.com>
2022-12-27 12:21:14 +0700

    Add customer data test

--- cypress/e2e/loginCustomer.cy.js
+++ cypress/e2e/loginCustomer.cy.js
@@ -117,9 +117,90 @@ describe('login test customer', () => {
     cy.get('[ng-submit="withdrawl()"]').submit();
     cy.get('[ng-show="message"]').should('have.text', 'Transaction successful');
     cy.get('[ng-hide="noAccount"]').find('.ng-binding').contains('3000');
-
+    //check transaction
     cy.get('[ng-click="transactions()"]').click();
+    // logout
+    cy.get('ng-click="byebye()"').click();
+  });
 
-    // cy.get('[ng-click="back()"]').click();
+  it('Ensure login customer account as Ron Weasly success', () => {
+    cy.get('[ng-click ="customer()"]').click();
+    cy.url().should(
+      'eq',
+      'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/customer'
+    );
+    cy.get('[name="userSelect"]').select('Ron Weasly');
+    cy.get('[name="myForm"]').submit();
+    // check selected name appear in page
+    cy.get('strong').find('span').should('contain', 'Ron Weasly');
+    cy.get('[ng-click="deposit()"]').click();
+    cy.get('[ng-submit="deposit()"]').find('input').type('5000');
+    cy.get('[ng-submit="deposit()"]').submit();
+    //  show Deposit Successful message
+    cy.get('[ng-show="message"]').should('have.text', 'Deposit Successful');
+    cy.get('[ng-hide="noAccount"]').find('.ng-binding').contains('5000');
+    cy.get('[ng-click="withdrawl()"]').click();
+    cy.get('[ng-submit="withdrawl()"]').find('input').type('2000');
+    cy.get('[ng-submit="withdrawl()"]').submit();
+    cy.get('[ng-show="message"]').should('have.text', 'Transaction successful');
+    cy.get('[ng-hide="noAccount"]').find('.ng-binding').contains('3000');
+    //check transaction
+    cy.get('[ng-click="transactions()"]').click();
+    // logout
+    cy.get('ng-click="byebye()"').click();
+  });
+
+  it('Ensure login customer account as Albus Dumbledore success', () => {
+    cy.get('[ng-click ="customer()"]').click();
+    cy.url().should(
+      'eq',
+      'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/customer'
+    );
+    cy.get('[name="userSelect"]').select('Albus Dumbledore');
+    cy.get('[name="myForm"]').submit();
+    // check selected name appear in page
+    cy.get('strong').find('span').should('contain', 'Albus Dumbledore');
+    cy.get('[ng-click="deposit()"]').click();
+    cy.get('[ng-submit="deposit()"]').find('input').type('5000');
+    cy.get('[ng-submit="deposit()"]').submit();
+    //  show Deposit Successful message
+    cy.get('[ng-show="message"]').should('have.text', 'Deposit Successful');
+    cy.get('[ng-hide="noAccount"]').find('.ng-binding').contains('5000');
+    cy.get('[ng-click="withdrawl()"]').click();
+    cy.get('[ng-submit="withdrawl()"]').find('input').type('2000');
+    cy.get('[ng-submit="withdrawl()"]').submit();
+    cy.get('[ng-show="message"]').should('have.text', 'Transaction successful');
+    cy.get('[ng-hide="noAccount"]').find('.ng-binding').contains('3000');
+    //check transaction
+    cy.get('[ng-click="transactions()"]').click();
+    // logout
+    cy.get('ng-click="byebye()"').click();
+  });
+
+  it('Ensure login customer account as Neville Longbottom success', () => {
+    cy.get('[ng-click ="customer()"]').click();
+    cy.url().should(
+      'eq',
+      'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/customer'
+    );
+    cy.get('[name="userSelect"]').select('Neville Longbottom');
+    cy.get('[name="myForm"]').submit();
+    // check selected name appear in page
+    cy.get('strong').find('span').should('contain', 'Neville Longbottom');
+    cy.get('[ng-click="deposit()"]').click();
+    cy.get('[ng-submit="deposit()"]').find('input').type('5000');
+    cy.get('[ng-submit="deposit()"]').submit();
+    //  show Deposit Successful message
+    cy.get('[ng-show="message"]').should('have.text', 'Deposit Successful');
+    cy.get('[ng-hide="noAccount"]').find('.ng-binding').contains('5000');
+    cy.get('[ng-click="withdrawl()"]').click();
+    cy.get('[ng-submit="withdrawl()"]').find('input').type('2000');
+    cy.get('[ng-submit="withdrawl()"]').submit();
+    cy.get('[ng-show="message"]').should('have.text', 'Transaction successful');
+    cy.get('[ng-hide="noAccount"]').find('.ng-binding').contains('3000');
+    //check transaction
+    cy.get('[ng-click="transactions()"]').click();
+    // logout
+    cy.get('ng-click="byebye()"').click();
   });
 });
